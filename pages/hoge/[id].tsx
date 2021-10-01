006186ebd6badc700d321d29afbeb45e68d6bf19
be.with.9078182 <be.with.9078182@gmail.com>
2021-10-01 18:27:04 +0900

    #comment add query param

--- pages/hoge/[id].tsx
+++ pages/hoge/[id].tsx
@@ -1,9 +1,12 @@
+import { useRouter } from 'next/router'
 import Layout from '../../components/layout'
 
 export default function HogeId() {
+  const router = useRouter()
+  const { id } = router.query
   return (
     <Layout>
-      <article>/hoge/[id]</article>
+      <article>/hoge/{id}</article>
     </Layout>
   )
 }
